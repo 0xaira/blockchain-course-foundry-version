f80f94294070038f696104ca08ddb3cf5651e8dd
Arsenic <airajena07@gmail.com>
2023-06-26 09:00:21 +0530

    Arrays and Structs

--- contracts/simpleStorage.sol
+++ contracts/simpleStorage.sol
@@ -1,11 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0
+// pragma solidity >=0.7.0 <0.9.0;
+// contract SimpleStorage {
+//     uint256 favoriteNumber;
+//     function store(uint _favoriteNumber) public{
+//               favoriteNumber= _favoriteNumber;
+//     }
+//     function retrive() public view returns (uint256) {
+//         return favoriteNumber;
+//     }
+// }
+
 pragma solidity >=0.7.0 <0.9.0;
 contract SimpleStorage {
-    uint256 favoriteNumber;
+    uint256 myFavoriteNumber;
+    struct Person{
+        string name;
+        uint favoriteNumber;
+    }
+    Person[] public listOfPeople;
     function store(uint _favoriteNumber) public{
-              favoriteNumber= _favoriteNumber;
+              myFavoriteNumber = _favoriteNumber;
     }
     function retrive() public view returns (uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
+    }
+    function addPerson(string memory _name, uint _favoriteNumber) public {
+        listOfPeople.push(Person(_name,_favoriteNumber));
     }
 }
