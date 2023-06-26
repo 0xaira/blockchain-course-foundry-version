e7f708ea43f01afbace89a0fbfaaf6d5b89dd96a
Arsenic <airajena07@gmail.com>
2023-06-26 09:17:54 +0530

    Mappings

--- contracts/simpleStorage.sol
+++ contracts/simpleStorage.sol
@@ -10,21 +10,46 @@
 //     }
 // }
 
-pragma solidity >=0.7.0 <0.9.0;
-contract SimpleStorage {
-    uint256 myFavoriteNumber;
-    struct Person{
-        string name;
-        uint favoriteNumber;
-    }
-    Person[] public listOfPeople;
-    function store(uint _favoriteNumber) public{
-              myFavoriteNumber = _favoriteNumber;
-    }
-    function retrive() public view returns (uint256) {
-        return myFavoriteNumber;
-    }
-    function addPerson(string memory _name, uint _favoriteNumber) public {
-        listOfPeople.push(Person(_name,_favoriteNumber));
-    }
-}
+// Arrays & Structs
+// pragma solidity >=0.7.0 <0.9.0;
+// contract SimpleStorage {
+//     uint256 myFavoriteNumber;
+//     struct Person{
+//         string name;
+//         uint favoriteNumber;
+//     }
+//     Person[] public listOfPeople;
+//     function store(uint _favoriteNumber) public{
+//               myFavoriteNumber = _favoriteNumber;
+//     }
+//     function retrive() public view returns (uint256) {
+//         return myFavoriteNumber;
+//     }
+//     function addPerson(string memory _name, uint _favoriteNumber) public {
+//         listOfPeople.push(Person(_name,_favoriteNumber));
+//     }
+// }
+
+// Mappings and Storage Types
+// pragma solidity >=0.7.0 <0.9.0;
+// contract SimpleStorage {
+//     uint256 myFavoriteNumber;
+//     struct Person{
+//         string name;
+//         uint favoriteNumber;
+//     }
+//     Person[] public listOfPeople;
+
+//     mapping(string=> uint256) public nameToFavoriteNumber;
+
+//     function store(uint _favoriteNumber) public{
+//               myFavoriteNumber = _favoriteNumber;
+//     }
+//     function retrive() public view returns (uint256) {
+//         return myFavoriteNumber;
+//     }
+//     function addPerson(string memory _name, uint _favoriteNumber) public {
+//         listOfPeople.push(Person(_name,_favoriteNumber));
+//         nameToFavoriteNumber[_name]= _favoriteNumber;
+//     }
+// }
